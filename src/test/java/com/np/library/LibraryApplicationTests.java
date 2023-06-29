f1f6c4245028594e9aa42401578ca06662b6d62b
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-29 23:02:30 +0200

    Projekat je inicijalizovan. Napravljena je konekcija za MySql bazom. Program je pokrenut i radi

---
+++ src/test/java/com/np/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
