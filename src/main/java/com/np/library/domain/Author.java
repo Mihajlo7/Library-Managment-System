aac0985276f867beae738f01e0256fe4c47ceb79
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-07-08 15:54:10 +0200

    Dokumentovana je klasa Author

--- src/main/java/com/np/library/domain/Author.java
+++ src/main/java/com/np/library/domain/Author.java
@@ -10,6 +10,11 @@ import lombok.NoArgsConstructor;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * @author Mihajlo Pavlovic
+ * Klasa opisujue autora
+ *
+ */
 @Entity
 @Table(name = "author")
 @NoArgsConstructor
@@ -17,20 +22,42 @@ import java.util.Set;
 @Getter
 @Builder
 public class Author {
+    /**
+     * jedinstveni identifikator autora
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    /**
+     * ime i prezime autora
+     */
     private String name;
+    /**
+     * biografija autora
+     */
     private String biography;
+    /**
+     * knjige koje je napisao
+     */
     @JsonIgnore
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
+    /**
+     * Konstruktor koji prima ime i biografiju
+     * @param name
+     * @param biography
+     */
     public Author(String name, String biography) {
         this.name = name;
         this.biography = biography;
     }
 
+    /**
+     * Postavlja ime autora
+     * @param name
+     * @throws IllegalArgumentException ako je name null ili prazan string
+     */
     public void setName(String name) {
         if(name==null){
             throw new IllegalArgumentException("The name must not have a null value");
@@ -41,6 +68,10 @@ public class Author {
         this.name = name;
     }
 
+    /**
+     * Postavlja biografiju autora
+     * @param biography
+     */
     public void setBiography(String biography) {
 //        if(biography==null){
 //            throw new IllegalArgumentException("The biography must not have a null value");
@@ -51,10 +82,18 @@ public class Author {
         this.biography = biography;
     }
 
+    /**
+     * Postavlja knjige koje je napisao autor
+     * @param books
+     */
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
 
+    /**
+     * ToString metoda
+     * @return Vrednost atributa autora kao string
+     */
     @Override
     public String toString() {
         return "Author{" +
@@ -64,6 +103,11 @@ public class Author {
                 '}';
     }
 
+    /**
+     * Poredi dva objekta autora
+     * @param o objekat sa kojim se poredi
+     * @return true ako su objekti isti ili sa istim vrednostima, false inace
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
